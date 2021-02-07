d0235ab56ce6cbc74598d97358ef1a7fe5e9ed0f
Hemanga26 <hemangabora62@gmail.com>
2021-02-07 14:28:59 +0530

    upload

---
+++ Register Form/login.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>HR Resort
+  </title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.0.0/css/font-awesome.css">
+</head>
+<body>
+
+  <!--Nevbar-->
+
+  <nav class="navbar navbar-expand-lg navbar-light fixed-top">
+    <div class="container">
+      <a class="navbar-brand" href="index.html">THE RIVERWAYS RETREAT</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="/">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">About Us</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Rooms</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Conference Hall</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Contact Us</a>
+      </li>
+      </ul>
+    
+  </div>
+    </div>
+  </nav>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+
+<center>
+<h2>LOG IN to see the available rooms</h2>
+
+
+ <!--cards-->
+ <div class="container">
+  <div class="row">
+    <div class="col mx-auto">
+     <div class="cards" id="room">
+       <form class="loginfrm">
+           <div class="form-group">
+             <label for="exampleInputEmail1"><h3>Email address</h3></label>
+             <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+             <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+           </div>
+           <div class="form-group">
+             <label for="exampleInputPassword1"><h3>Password</h3></label>
+             <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+           </div>
+           <div class="form-check">
+             <input type="checkbox" class="form-check-input" id="exampleCheck1">
+             <label class="form-check-label" for="exampleCheck1">Check me out</label>
+           </div><br>
+           <button type="submit" class="btn btn-primary">Submit</button>
+         </form>
+   </div>
+    </div>
+  </div>
+ </div>
+ 
+
+
+<h3> Already have an account?</h3>   <button type="sign up" class="btn btn-primary" href ="#sign up.php">SIGN UP</button>
+
+</center>
+<br>
+<br>
+<br>
+<!--footer-->
+
+<footer>
+  <div class="footer-bottom">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-3 col-sm-6 col-xs-12 segment-one">
+          <h2>About Us</h2>
+          <p>The place that gives the riverside vibe with a homely environment for everyone</p>
+        </div>
+        <div class="col-md-3 col-sm-6 col-xs-12 segment-two">
+          <h2>Contact Us</h2>
+         <ul>
+           <li><a href="#"></a>Email us at theriverways@gmail.com</li>
+           <li><a href="#"></a>call us at 0373-2367549</li>
+         </ul>
+        </div>
+        <div class="col-md-3 col-sm-6 col-xs-12 segment-three">
+          <h2>Follow Us </h2>
+          <p>Plese follow us on our social media profile</p>
+          <a href="https://www.facebook.com/upasana.sarmah.92"><i class="fa fa-facebook" style="font-size: 150%;"></i></a>
+          <a href="https://twitter.com/RKrishnatra"><i class="fa fa-twitter" style="font-size: 150%;"></i></a>
+          <a href="https://www.instagram.com/rabinasharma_/"><i class="fa fa-instagram" style="font-size: 150%;"></i></a>
+        </div>
+        
+      </div>
+    </div>
+
+  </div>
+</footer>
+
+
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"></script>
+
+</body>
+</html>
+
+
